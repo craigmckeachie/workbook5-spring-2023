1ea8e8b9a307681751037f7a598ae76b29d376cd
Craig McKeachie <craigdmckeachie@gmail.com>
2023-05-16 15:41:10 -0400

    cheap candy updates

--- exercises/es6scripts/es6_cheap_candy.js
+++ exercises/es6scripts/es6_cheap_candy.js
@@ -3,9 +3,17 @@ let products = [
   { product: "Plain M&Ms", price: 2.89 },
   { product: "Peanut M&Ms", price: 2.89 },
   { product: "Swedish Fish", price: 3.79 },
-  // TODO: fill the array with 10 candies of various
-  // price ranges
+  { product: "Reeses Peanut Butter Cups", price: 5.38 },
+  { product: "Krackle", price: 4.12 },
+  { product: "Starbursts", price: 2.52 },
+  { product: "Skittles", price: 7.79 },
+  { product: "Reeses Pieces", price: 3.23 },
+  { product: "Three Musketeers", price: 4.89 },
 ];
+
+console.log("===========Products sorted by price==============");
+products.sort((a, b) => a.price - b.price);
+console.log(products);
 
 console.log("===========Which candies costs less than $4.00?==============");
 let cheapProducts = products.filter((p) => p.price < 4);
